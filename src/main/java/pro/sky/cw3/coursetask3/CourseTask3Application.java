ae0e1705737723e30b4b2510d41d171c3aa83d11
yarik <yarikshtoda1988@gmail.com>
2023-02-02 17:08:56 +0300

    CourseTask3

---
+++ src/main/java/pro/sky/cw3/coursetask3/CourseTask3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.cw3.coursetask3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseTask3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseTask3Application.class, args);
+    }
+
+}
